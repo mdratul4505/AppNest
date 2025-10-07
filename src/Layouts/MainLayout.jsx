b57ae61959e0558855e0b05a4b69e240f3511224
mdratul4505 <dev.raul.coder@gmail.com>
2025-10-08 02:42:39 +0600

    added footer section

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -5,12 +5,14 @@ import { Outlet } from 'react-router';
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className='flex flex-col min-h-screen'>
 
             <Navbar></Navbar>
 
-            <Outlet></Outlet>
-            
+            <div className='flex-1'>
+                <Outlet></Outlet>
+            </div>
+
             <Footer></Footer>
             
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router';
+import playStore from '../assets/fi_16076057.png'
+import AppStore from '../assets/Group.png'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className=''>
+            <div className='text-center mt-20'>
+            <h1 className='font-bold text-7xl'>We Build <br />
+                <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#632EE3] to-[#9F62F2]'>Productive</span> Apps</h1>
+
+            <p className='text-gray-600 my-8'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.<br /> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>  
+
+            <div className='flex items-center justify-center my-8 gap-5 '>
+                <Link className='flex items-center gap-2.5 btn' to='https://play.google.com/store/games?hl=en'><img src={playStore} alt="" /> Google Play</Link>
+                <Link className='flex items-center gap-2.5 btn' to=''> <img src={AppStore} alt="" /> App Store</Link>
+            </div>  
+
+        </div>
+        {/* banner section */}
+        
         </div>
     );
 };
